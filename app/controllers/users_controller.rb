0d09a2d9a52f26a1dfcf3ec9deaf61c3a4c7c62d
Amanda <main@Amandas-MacBook-Air.local>
2020-03-28 16:35:18 -0400

    added name column to users table, added app/helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,25 @@
 class UsersController < ApplicationController
 
   get '/signup' do 
-    erb :'users/signup'
+    if Helpers.is_logged_in?(session)
+      user = Helpers.current_user(session)
+      redirect to "/users/#{user.id}" 
+    else 
+      erb :'users/signup'
+    end
   end 
   
   get '/login' do 
-    erb :'users/login'
+    if Helpers.is_logged_in?(session)
+      user = Helpers.current_user(session)
+      redirect to "/users/#{user.id}" 
+    else
+      erb :'users/login'
+    end
   end 
 
   post '/login' do  
-    user = User.find_by(username params[:username])
+    user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password]) 
       session[:user_id] = user.id 
       redirect to "/users/#{user.id}"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200327204048) do
+ActiveRecord::Schema.define(version: 20200328191049) do
 
   create_table "languages", force: :cascade do |t|
     t.string  "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20200327204048) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.string "name"
   end
 
 end
